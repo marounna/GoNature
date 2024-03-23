7601480e60009f82b8ca7e7ed8b0fdae01b189eb
AdarCohen1 <Adarcohen1@gmail.com>
2024-03-23 22:09:56 +0200

    commit 2

--- GoNatureClient/src/clientGUI/ParkManagerMenuController.java
+++ GoNatureClient/src/clientGUI/ParkManagerMenuController.java
@@ -81,11 +81,6 @@ public class ParkManagerMenuController {
     }
 
     @FXML
-    void ClickOnLogoutBtn(ActionEvent event) {
-
-    }
-
-    @FXML
     void ClickOnOkDwell(ActionEvent event) {
 
     }
