551fa0571d678d6e715e66f712cb3bbde3d8a4be
AdarCohen1 <Adarcohen1@gmail.com>
2024-03-23 22:46:00 +0200

    commit 3

--- GoNatureClient/src/clientGUI/DmMenuController.java
+++ GoNatureClient/src/clientGUI/DmMenuController.java
@@ -9,53 +9,119 @@ import common.SwitchScreen;
 import entities.Park;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
-import logic.Message;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+
 
 public class DmMenuController {
 	
-	public static  String[] parks;
+	    @FXML
+	    private Button logOutBtn;
 
-    
-    @FXML
-    public void initialize() {
-    	getParks();
-    }
+	    @FXML
+	    private TableColumn<?, ?> maxAfterCapacityTable;
 
-    private void getParks() {
-    	  ClientUI.chat.accept("park");
-    	  parks=new String[StaticClass.parks.size()];
-    	  for(int i=0;i<StaticClass.parks.size();i++) {
-    		  parks[i]=StaticClass.parks.get(i).getParkString();
-    	  }
-    	  
+	    @FXML
+	    private TableColumn<?, ?> maxAfterDwellTable;
+
+	    @FXML
+	    private TableColumn<?, ?> maxBeforeCapacityTable;
+
+	    @FXML
+	    private TableColumn<?, ?> maxBeforeDwellTable;
+
+	    @FXML
+	    private TableColumn<?, ?> noCapacityTable;
+
+	    @FXML
+	    private TableColumn<?, ?> noDwellTable;
+
+	    @FXML
+	    private TableColumn<?, ?> parkNameCapacityTable;
+
+	    @FXML
+	    private TableColumn<?, ?> parkNameDwellTable;
+
+	    @FXML
+	    private TableColumn<?, ?> yesCapacityTable;
+
+	    @FXML
+	    private TableColumn<?, ?> yesDwellTable;
+	    
+	    
+	    public static  String[] parks;
+
+	    
+	    @FXML
+	    public void initialize() {
+	    	  ClientUI.chat.accept("park");
+	    	  parks=new String[StaticClass.parks.size()];
+	    	  for(int i=0;i<StaticClass.parks.size();i++) {
+	    		  parks[i]=StaticClass.parks.get(i).getParkString();
+	    	  }
+	    }
+
+	    @FXML
+	    void CancelledReport(ActionEvent event) {
+	    	SwitchScreen.changeScreen(event, "/clientGUI/DmCancelledReport.fxml","/resources/DmCancelledReport.css");
+
+	    }
+
+
+	    @FXML
+	    void VisitReport(ActionEvent event) {
+	    	SwitchScreen.changeScreen(event, "/clientGUI/DmVisitReport.fxml", "/resources/DmVisitReport.css");
+	    }
+	    
+	    @FXML
+	    void clickOnLogout(ActionEvent event) {
+			try {
+				ClientUI.chat.accept("logout "+StaticClass.username);
+				System.out.println("DmMenuController> Logout request Sent to server");
+			}catch (Exception e){
+				e.printStackTrace();
+				System.out.println("error"+e.getMessage());
+			}
+			if(StaticClass.islogout) {
+				StaticClass.islogout=false;
+		        
+	    		SwitchScreen.changeScreen(event,"/clientGUI/LoginController.fxml", "/resources/LoginController.css");
+			}
+	    }
+
 	}
-
-	@FXML //user logs out, moving to login screen
-    void ClickOnLogOutBtn(ActionEvent event) throws IOException {
-    	String message="logout "+StaticClass.username;
-		try {
-			ClientUI.chat.accept(message);
-			System.out.println("DmMenuController> Logout request Sent to server");
-		}catch (Exception e){
-			e.printStackTrace();
-			System.out.println("error"+e.getMessage());
-		}
-		if(StaticClass.islogout) {
-			StaticClass.islogout=false;
-	        
-    		SwitchScreen.changeScreen(event,"/clientGUI/LoginController.fxml", "/resources/LoginController.css");
-		}
-    }
-    
 	
-    @FXML
-    void VisitReport(ActionEvent event) {
-    	SwitchScreen.changeScreen(event, "/clientGUI/DmVisitReport.fxml", "/resources/DmVisitReport.css");
-    }
-    
-    @FXML
-    void CancelledReport(ActionEvent event) {
-    	SwitchScreen.changeScreen(event, "/clientGUI/DmCancelledReport.fxml","/resources/DmCancelledReport.css");
-    }
-}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
