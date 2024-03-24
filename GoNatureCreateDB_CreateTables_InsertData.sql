ed6195c2326e399c743d942621b013725a93c2f4
marounna <137427615+marounna@users.noreply.github.com>
2024-03-24 09:31:18 +0200

    Update GoNatureCreateDB_CreateTables_InsertData.sql
    add  IsCanceledAuto field to orders table : default "NO"

--- GoNatureCreateDB_CreateTables_InsertData.sql
+++ GoNatureCreateDB_CreateTables_InsertData.sql
@@ -153,6 +153,7 @@ CREATE TABLE gonaturedb.orders (
     IsConfirmed VARCHAR(255),
     IsVisit VARCHAR(255),
     IsCanceled VARCHAR(255),
+    IsCanceledAuto VARCHAR(255) DEFAULT 'NO',
     TotalPrice VARCHAR(255),
     IsInWaitingList VARCHAR(255),
     SaleType VARCHAR(255),
@@ -163,16 +164,16 @@ CREATE TABLE gonaturedb.orders (
 
 
 
-INSERT INTO gonaturedb.orders ( ParkName, UserId, dateOfVisit,TimeOfVisit, NumberOfVisitors, IsConfirmed, IsVisit, IsCanceled, TotalPrice, IsInWaitingList) 
+INSERT INTO gonaturedb.orders ( ParkName, UserId, dateOfVisit,TimeOfVisit, NumberOfVisitors, IsConfirmed, IsVisit, IsCanceled, TotalPrice, IsInWaitingList,SaleType) 
 VALUES
-( 'Safari',            '314813825','2024-03-13','10:00', '5', 'YES', 'YES', 'NO', '300', 'NO'),
-( 'Mitspe-Tat-Yami',   '314813832','2024-03-13','10:00', '5', 'YES', 'YES', 'NO', '300', 'NO'),
-( 'Hay-Park',          '314813833','2024-03-12','13:00', '3', 'YES', 'YES', 'NO', '300', 'NO'),
-( 'Yaar-Hakofim',      '314813831','2024-03-12','12:00', '7', 'YES', 'YES', 'NO', '300', 'NO'),
-( 'Gan-Tanahi',        '314813832','2024-03-11','10:00', '5', 'YES', 'YES', 'NO', '300', 'NO'),
-( 'Safari',            '314813833','2024-03-11','17:00', '5', 'YES', 'YES', 'NO', '300', 'NO'),
-( 'Safari',            '314813834','2024-03-11','11:00', '1', 'YES', 'YES', 'NO', '300', 'NO'),
-( 'Yaar-Hakofim',      '314813831','2024-03-11','12:00', '3', 'YES', 'YES', 'NO', '300', 'NO');
+( 'Safari',            '314813825','2024-03-13','10:00', '5', 'YES', 'YES', 'NO', '300', 'NO',"casual_personal"),
+( 'Mitspe-Tat-Yami',   '314813832','2024-03-13','10:00', '5', 'YES', 'YES', 'NO', '300', 'NO',"casual_personal"),
+( 'Hay-Park',          '314813833','2024-03-12','13:00', '3', 'YES', 'YES', 'NO', '300', 'NO',"casual_group"),
+( 'Yaar-Hakofim',      '314813831','2024-03-12','12:00', '7', 'YES', 'YES', 'NO', '300', 'NO',"group"),
+( 'Gan-Tanahi',        '314813832','2024-03-11','10:00', '5', 'YES', 'YES', 'NO', '300', 'NO',"personal"),
+( 'Safari',            '314813833','2024-03-11','17:00', '5', 'YES', 'YES', 'NO', '300', 'NO',"casual_group"),
+( 'Safari',            '314813834','2024-03-11','11:00', '1', 'YES', 'YES', 'NO', '300', 'NO',"casual_personal"),
+( 'Yaar-Hakofim',      '314813831','2024-03-11','12:00', '3', 'YES', 'YES', 'NO', '300', 'NO',"casual_group");
 
 
 
@@ -188,11 +189,13 @@ VALUES
     ('Mitspe-Tat-Yami', '2024-03-17', 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0);
 
 CREATE TABLE gonaturedb.report_visit (
-    ReportId VARCHAR(255) PRIMARY KEY,
+    ReportId int auto_increment PRIMARY KEY,
     ParkName VARCHAR(255),
     Year VARCHAR(255),
     Month VARCHAR(255),
-    NumberOfVisitors VARCHAR(255),
+    NumberOfVisitorsTotal VARCHAR(255),
+    NumberOfVisitorsGroups	VARCHAR(255),
+	NumberOfVisitorsIndividual	VARCHAR(255),
     FOREIGN KEY (ParkName) REFERENCES park(Parkname)
 );
 
@@ -205,28 +208,3 @@ CREATE TABLE gonaturedb.report_cancellation (
     OrdersNotVisited VARCHAR(255),
     FOREIGN KEY (ParkName) REFERENCES park(Parkname)
 );
-
-
-
-INSERT INTO gonaturedb.report_visit (ReportId, ParkName, Year, Month, NumberOfVisitors) 
-VALUES 
-('1000', 'Yaar-Hakofim',       '2023', '10', '1'),
-('1001', 'Safari',             '2023', '09', '1'),
-('1002', 'Mitspe-Tat-Yami',    '2022', '04', '1'),
-('1003', 'Yaar-Hakofim',       '2023', '04', '5'),
-('1004', 'Safari',             '2022', '11', '3'),
-('1005', 'Safari',             '2022', '12', '7'),
-('1006', 'Yaar-Hakofim',       '2022', '03', '12'),
-('1007', 'Gan-Tanahi',         '2023', '09', '6'),
-('1008', 'Hay-Park',           '2023', '04', '15'),
-('1009', 'Safari',             '2022', '05', '1'),
-('1010', 'Safari',             '2022', '04', '4'),
-('1011', 'Safari',             '2022', '06', '2'),
-('1012', 'Mitspe-Tat-Yami',    '2022', '07', '1'),
-('1013', 'Gan-Tanahi',         '2023', '10', '7'),
-('1014', 'Safari',             '2022', '05', '2'),
-('1015', 'Safari',             '2023', '09', '6'),
-('1016', 'Yaar-Hakofim',       '2023', '12', '6'),
-('1017', 'Gan-Tanahi',         '2022', '02', '7'),
-('1018', 'Hay-Park',           '2022', '11', '14'),
-('1019', 'Yaar-Hakofim',       '2022', '04', '4');
